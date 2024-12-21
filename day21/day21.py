f0bd4058396a48d603293e7b1acca3462997a23c
spindot8 <spindot@gmx.net>
2024-12-21 21:06:24 +0100

    AoC 2024 day 21 further cleanup

--- day21/day21.py
+++ day21/day21.py
@@ -1,5 +1,6 @@
 import time
 import itertools
+from collections import *
 import pyperclip
 
 
@@ -24,6 +25,7 @@ num_pad = {'7': (0, 0), '8': (1, 0), '9': (2, 0),
            '4': (0, 1), '5': (1, 1), '6': (2, 1),
            '1': (0, 2), '2': (1, 2), '3': (2, 2),
                         '0': (1, 3), 'A': (2, 3)}
+rev_num_pad = {v: k for k, v in num_pad.items()}
 
 '''
     +---+---+
@@ -34,19 +36,18 @@ num_pad = {'7': (0, 0), '8': (1, 0), '9': (2, 0),
 '''
 robo_pad = {             '^': (1, 0), 'A': (2, 0),
             '<': (0, 1), 'v': (1, 1), '>': (2, 1)}
+rev_robo_pad = {v: k for k, v in robo_pad.items()}
 
 
 def solve_puzzle(filename, param=None, verbose=False):
     lines = [line.strip('\n') for line in open(filename, 'r').readlines()]
 
-    p1, p2 = 0, 0
-
-    inp = []
+    codes = []
     for y, line in enumerate(lines):
-        inp.append(line)
+        codes.append(line)
 
-    moves_num_pad = calc_pad_moves(num_pad)
-    moves_robo_pad = calc_pad_moves(robo_pad)
+    moves_num_pad = calc_pad_moves(rev_num_pad)
+    moves_robo_pad = calc_pad_moves(rev_robo_pad)
     length_moves_robo_pad = {k: len(v[0]) for k, v in moves_robo_pad.items()}
     cache = {}
 
@@ -63,107 +64,73 @@ def solve_puzzle(filename, param=None, verbose=False):
         else:
             prev_digit = 'A'
             for digit in code:
-                length_list = []
-                if (prev_digit, digit) not in moves_robo_pad:
-                    print(code, loop, prev_digit, digit)
+                next_min_len = int(1e12)
                 for next_code in moves_robo_pad[(prev_digit, digit)]:
-                    length_list.append(calc_length(next_code, loop - 1))
+                    next_min_len = min(next_min_len, calc_length(next_code, loop - 1))
                 prev_digit = digit
-                min_length += min(length_list)
+                min_length += next_min_len
 
         cache[(code, loop)] = min_length
         return min_length
 
-    def calc_min_length(num_pad_moves, loop):
-        length_list = []
-        for move in num_pad_moves:
-            length_list.append(calc_length(move, loop))
-        min_len = min(length_list)
-        return min_len
-
-    for code in inp:
+    p1, p2 = 0, 0
+    for code in codes:
         num = int(code[:-1])
-        num_pad_moves = calc_moves(code, moves_num_pad)
-        min_len_p1 = calc_min_length(num_pad_moves, 2)
+        num_pad_moves = calc_num_pad_moves(code, moves_num_pad)
+
+        min_len_p1 = min(calc_length(move, 2) for move in num_pad_moves)
         p1 += min_len_p1 * num
-        min_len_p2 = calc_min_length(num_pad_moves, 25)
+
+        min_len_p2 = min(calc_length(move, 25) for move in num_pad_moves)
         p2 += min_len_p2 * num
-        # print(code, num, min_len_p1, min_len_p2)
 
     return p1, p2
 
 
-def calc_moves(code, optimal_moves_pad):
+def calc_num_pad_moves(code: str, moves_num_pad: dict):
     moves = []
     prev_digit = 'A'
     for digit in code:
-        moves.append(optimal_moves_pad[(prev_digit, digit)])
+        moves.append(moves_num_pad[(prev_digit, digit)])
         prev_digit = digit
-    moves = itertools.product(*moves)
-    moves = [''.join(move) for move in moves]
+    moves = [''.join(move) for move in itertools.product(*moves)]
     return moves
 
 
-def calc_pad_moves(pad):
+def calc_pad_moves(rev_pad: dict):
     optimal_moves = {}
-    for d1, p1 in pad.items():
-        for d2, p2 in pad.items():
+    for p1, d1 in rev_pad.items():
+        for p2, d2 in rev_pad.items():
             if d1 == d2:
                 optimal_moves[(d1, d2)] = ['A']
             else:
-                cx, cy = p1
-                tx, ty = p2
-                dx, dy = tx - cx, ty - cy
-                possibilities = move_pad_once(pad, cx, cy, dx, dy, [[]])
-                possibilities = [''.join(possibility) for possibility in possibilities]
+                possibilities = possible_pad_moves_between_two_keys(rev_pad, p1, p2)
                 optimal_moves[(d1, d2)] = possibilities
     return optimal_moves
 
 
-def move_pad_once(pad, cx, cy, dx, dy, possibilities):
-    if dx == 0 and dy == 0:
-        for possi in possibilities:
-            possi.append('A')
-        return possibilities
+def possible_pad_moves_between_two_keys(rev_pad: dict, start_pos, target_pos):
+    possibilities = []
 
-    new_possibilities = []
-    if dx > 0:
-        nx, ny = cx + 1, cy
-        if (nx, ny) in pad.values():
-            step_new_possis = []
-            for possi in possibilities:
-                new_possi = list(possi)
-                new_possi.append('>')
-                step_new_possis.append(new_possi)
-            new_possibilities.extend(move_pad_once(pad, nx, ny, dx - 1, dy, step_new_possis))
-    if dx < 0:
-        nx, ny = cx - 1, cy
-        if (nx, ny) in pad.values():
-            step_new_possis = []
-            for possi in possibilities:
-                new_possi = list(possi)
-                new_possi.append('<')
-                step_new_possis.append(new_possi)
-            new_possibilities.extend(move_pad_once(pad, nx, ny, dx + 1, dy, step_new_possis))
-    if dy > 0:
-        nx, ny = cx, cy + 1
-        if (nx, ny) in pad.values():
-            step_new_possis = []
-            for possi in possibilities:
-                new_possi = list(possi)
-                new_possi.append('v')
-                step_new_possis.append(new_possi)
-            new_possibilities.extend(move_pad_once(pad, nx, ny, dx, dy - 1, step_new_possis))
-    if dy < 0:
-        nx, ny = cx, cy - 1
-        if (nx, ny) in pad.values():
-            step_new_possis = []
-            for possi in possibilities:
-                new_possi = list(possi)
-                new_possi.append('^')
-                step_new_possis.append(new_possi)
-            new_possibilities.extend(move_pad_once(pad, nx, ny, dx, dy + 1, step_new_possis))
-    return new_possibilities
+    min_length = int(1e6)
+    sx, sy = start_pos
+    q = deque([(sx, sy, '')])
+    while q:
+        cx, cy, path = q.popleft()
+        for dx, dy, dm in [(0, -1, '^'), (1, 0, '>'), (0, 1, 'v'), (-1, 0, '<')]:
+            nx, ny = cx + dx, cy + dy
+            if (nx, ny) in rev_pad:
+                new_path = path + dm
+                if (nx, ny) == target_pos:
+                    min_length = min(min_length, len(new_path))
+                    if len(new_path) <= min_length:
+                        possibilities.append(new_path + 'A')
+                    else:
+                        break
+                else:
+                    if len(new_path) < min_length:
+                        q.append((nx, ny, new_path))
+    return possibilities
 
 
 def main():
