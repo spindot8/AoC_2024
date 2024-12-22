2b2a0a0b686d7068d315c28b8593d229af5501ab
spindot8 <spindot@gmx.net>
2024-12-22 13:05:08 +0100

    AoC 2024 day 22 optimized

--- day22/day22.py
+++ day22/day22.py
@@ -1,5 +1,5 @@
 import time
-import itertools
+from collections import defaultdict
 import pyperclip
 
 
@@ -14,56 +14,35 @@ def solve_puzzle(filename, param=None, verbose=False):
 
     p1, p2 = 0, 0
 
-    inp = []
-    for y, line in enumerate(lines):
-        inp.append(int(line))
-
-    prices_list = []
-    diffs_list = []
-    for n in inp:
+    results = defaultdict(int)
+    for line in lines:
+        n = int(line)
         prices = [n % 10]
+        diffs = []
         for loop in range(2000):
             n = (n ^ (n * 64)) % 16777216
             n = (n ^ (n // 32)) % 16777216
             n = (n ^ (n * 2048)) % 16777216
             prices.append(n % 10)
+            diffs.append(prices[-1] - prices[-2])
         p1 += n
-        prices_list.append(prices)
-        diffs = [b - a for a, b in zip(prices, prices[1:])]
-        diffs_list.append(diffs)
 
-    possible_diffs = set()
-    for diffs in diffs_list:
+        seen = set()
         for idx in range(3, len(diffs)):
-            possible_diffs.add(tuple(diffs[idx - 3:idx + 1]))
-    # print(len(possible_diffs))
-    # print(len(list(itertools.product([n for n in range(-9, 10)], repeat=4))))
+            diff_seq = tuple(diffs[idx - 3:idx + 1])
+            if diff_seq not in seen:
+                seen.add(diff_seq)
+                results[diff_seq] += prices[idx + 1]
 
-    pre_process_diffs_list = []
-    for m, diff in enumerate(diffs_list):
-        pre_process_diff = {}
-        for idx in range(3, len(diff)):
-            cdiff = tuple(diff[idx - 3:idx + 1])
-            if cdiff not in pre_process_diff:
-                pre_process_diff[cdiff] = prices_list[m][idx + 1]
-        pre_process_diffs_list.append(pre_process_diff)
-
-    for pd in possible_diffs:
-        ans = 0
-        for pre_process_diffs in pre_process_diffs_list:
-            if pd in pre_process_diffs:
-                ans += pre_process_diffs[pd]
-        if ans > p2:
-            # print(ans, pd)
-            p2 = ans
+    # print(len(results))
+    p2 = max(results.values())
 
     return p1, p2
 
 
 def main():
     input_data_list = [
-        ['sample.txt',  'sample data  ', False, False, None],
-        # takes about 30 seconds
+        ['sample.txt',  'sample data  ', True, False, None],
         ['data.txt',    'real data    ', True,  False, None],
     ]
 
